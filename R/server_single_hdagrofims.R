a341f9d362f76a282ef150e1ebd7df89fba8a404
CIP-RIU <cip-riu@cgiar.org>
2018-06-26 21:00:45 +0000

    now available f2crd and f2rcbd

--- R/server_single_hdagrofims.R
+++ R/server_single_hdagrofims.R
@@ -38,32 +38,38 @@ single_hdagrofims_server <- function(input, output, session, values){
     out
   })
   
-  output$genotypes_single_agrofims  <- renderUI({
+  output$genotypes_single_agrofims  <- renderUI({ #genotypes
     selectInput('genotypes_single_agrofims', 'Select Treatments', c(Choose='', names(hot_fb_agrofims()) ), 
                 selectize=TRUE)
   })
   
-  output$rep_single_agrofims  <- renderUI({
+  output$rep_single_agrofims  <- renderUI({ #repetition
     selectInput('rep_single_agrofims', 'Select Replications', c(Choose='', names(hot_fb_agrofims())),
-                selectize=TRUE)
+                selectize=TRUE) 
   })
   
-  output$trait_single_agrofims <- renderUI({
+  output$trait_single_agrofims <- renderUI({ #trait
     selectInput('trait_single_agrofims', 'Select Trait(s)', c(Choose='', names(hot_fb_agrofims())),
                 selectize=TRUE, multiple = TRUE)
   })
   
-  output$factor_single_agrofims  <- renderUI({
-    selectInput('factor_single_agrofims', 'Select Factor', c(Choose='', names(hot_fb_agrofims()) ),
+  output$factor_single_agrofims  <- renderUI({ #factor 1
+    selectInput('factor_single_agrofims', 'Select Treatment Factor 1', c(Choose='', names(hot_fb_agrofims()) ),
                 selectize=TRUE)
   })
   
-  output$block_single_agrofims  <- renderUI({
+  output$factor2_single_agrofims  <- renderUI({ #factor 2
+    selectInput('factor_single_agrofims2', 'Select Treatment Factor 2', c(Choose='', names(hot_fb_agrofims()) ),
+                selectize=TRUE)
+  })
+  
+  
+  output$block_single_agrofims  <- renderUI({ #block
     selectInput('block_single_agrofims', 'Select Block', c(Choose='', names(hot_fb_agrofims()) ),
                 selectize=TRUE)
   })
   
-  output$k_single_agrofims  <- renderUI({
+  output$k_single_agrofims  <- renderUI({ #block size
     shiny::numericInput('k_single_agrofims', 'Select Block Size',   value =2, min=2, max = 100)
   })    
   
@@ -90,7 +96,6 @@ single_hdagrofims_server <- function(input, output, session, values){
   output$downloadagrofims_single_report <- downloadHandler(
     filename =  paste0("report", "_", as.character(Sys.time(), "%Y%m%d%H%M%S"), '.docx'),
     content = function(con) {
-      #content = function(file) {  
       
       shiny::withProgress(message = "Opening single Report...",value= 0,{
         
@@ -104,8 +109,9 @@ single_hdagrofims_server <- function(input, output, session, values){
         rep <- input$rep_single_agrofims
         genotypes <- input$genotypes_single_agrofims
         block <- input$block_single_agrofims
-        k <- input$k_single_agrofims
-        factor_single <- input$factor_single_agrofims
+        k <- input$k_single_agrofims #
+        factor_single <- input$factor_single_agrofims # factor 1
+        factor2_single <- input$factor_single_agrofims2 #factor 2
         
         #type of document
         format <- "word" #paste(input$format_single_agrofims, sep="")
@@ -116,7 +122,6 @@ single_hdagrofims_server <- function(input, output, session, values){
         
         if(design == "Randomized Complete Block Design (RCBD)"){
           
-          
           servName =   "rcbd"
           serverFileDir <-"https://research.cip.cgiar.org/gtdms/hidap/hidap_sbase_reports/files/"
           serverService <-"https://research.cip.cgiar.org/gtdms/hidap/hidap_sbase_reports/getFileUpload.php"
@@ -125,17 +130,12 @@ single_hdagrofims_server <- function(input, output, session, values){
           ranStr <-  stringi::stri_rand_strings(1, 15,  '[a-zA-Z0-9]')
           servName <- paste(uploadDate, ranStr, servName , sep= "-") #nombre sin extensions!!!!
           
-          #dirfiles <- system.file(package = "pepa")
-          
           dirName <- fbglobal::get_base_dir()
           print(dirName)
           path <- paste0(dirName, servName, ".docx")
           
-          #print(path)
-          
           try(pepa::repo.rcbd(traits = trait, geno = genotypes, rep = rep, format = format, data = fieldbook, server =TRUE, server_dir_name = dirName, 
                               server_file_name = servName))
-          #path<- "~/R/x86_64-pc-linux-gnu-library/3.4/pepa/rmd/rcbd.docx" #rsconnect cip
           
           params <- list(
             dataRequest = "uploadFile",
@@ -155,9 +155,6 @@ single_hdagrofims_server <- function(input, output, session, values){
           
           
         }
-        
-        
-        
         
         if(design == "Completely Randomized Design (CRD)"){
           
@@ -178,8 +175,8 @@ single_hdagrofims_server <- function(input, output, session, values){
           
           print(path)
           
-          try(pepa::repo.crd(traits = trait, geno = genotypes, format = format, data = fieldbook, server =TRUE, server_dir_name = dirName, 
-                             server_file_name = servName
+          try(pepa::repo.crd(traits = trait, geno = genotypes, format = format, data = fieldbook, 
+                             server =TRUE, server_dir_name = dirName, server_file_name = servName
           ))
           
           params <- list(
@@ -199,7 +196,6 @@ single_hdagrofims_server <- function(input, output, session, values){
             print("Not uploaded")
           
         }
-        
         
         if(design == "Augmented Block Design (ABD)"){
           
@@ -243,18 +239,10 @@ single_hdagrofims_server <- function(input, output, session, values){
           else
             print("Not uploaded")
           
-          
-          #try(pepa::repo.abd(traits = trait, geno = genotypes, rep = rep, format = format, data = fieldbook))
-          #path <- "/usr/local/lib/R/site-library/pepa/rmd/abd.docx" # # former shiny server CIP-RIU
-          #path<- "~/R/x86_64-pc-linux-gnu-library/3.4/pepa/rmd/abd.docx" #rsconnect cip
-          #tempReport <- file.path(tempdir(), "abd.docx") 
-          #file.copy("/usr/local/lib/R/site-library/pepa/rmd/abd.docx", con)
-          
-          
+     
           
         }
-        # 
-        # 
+        
         if(design == "Alpha design"){
           
           
@@ -304,47 +292,98 @@ single_hdagrofims_server <- function(input, output, session, values){
           
           
         }
-        # 
-        # 
-        # if(design == "Split Plot with Plots in CRD (SPCRD)"){
-        #   title <- paste("Automatic report for ", design, sep= "")
-        #   try(pepa::repo.2f(traits = trait, A = genotypes, B = factor_single, rep = rep, design = "crd",  title= title, data = fieldbook, format = format))
-        #   path <- "/usr/local/lib/R/site-library/pepa/rmd/2fcrd.docx"
-        #   #file.copy("/usr/local/lib/R/site-library/pepa/rmd/2fcrd.docx", con)
-        # }
-        # 
-        # 
-        # if(design == "Factorial Two-Way Design in CRD (F2CRD)"){
-        #   title <- paste("Automatic report for ", design, sep= "")
-        #   try(pepa::repo.2f(traits = trait, A = genotypes, B = factor_single, rep = rep, design = "crd",  title= title, data = fieldbook, format = format))
-        #   path <- "/usr/local/lib/R/site-library/pepa/rmd/2fcrd.docx"
-        #   #file.copy("/usr/local/lib/R/site-library/pepa/rmd/2fcrd.docx", con)
-        # }
-        # 
-        # 
+       
+        if(design == "Factorial Two-Way Design in CRD (F2CRD)"){
+          
+          servName =   "2fcrd"
+          serverFileDir <-"https://research.cip.cgiar.org/gtdms/hidap/hidap_sbase_reports/files/"
+          serverService <-"https://research.cip.cgiar.org/gtdms/hidap/hidap_sbase_reports/getFileUpload.php"
+          
+          uploadDate  <- as.character(Sys.time(), "%Y%m%d%H%M%S")
+          ranStr <-  stringi::stri_rand_strings(1, 15,  '[a-zA-Z0-9]')
+          servName <- paste(uploadDate, ranStr, servName , sep= "-") #nombre sin extensions!!!!
+          
+          dirName <- fbglobal::get_base_dir()
+          print(dirName)
+          path <- paste0(dirName, servName, ".docx")
+          
+          pepa::repo.2f(data = fieldbook, traits = trait, 
+                        A = factor_single , B = factor2_single,
+                        rep = rep, design = "crd",format = format, 
+                        server =TRUE, server_dir_name = dirName, server_file_name = servName )
+          
+          
+          params <- list(
+            dataRequest = "uploadFile",
+            fileServerName = paste0(servName, ".docx"),
+            filedata= httr::upload_file(path, "text/csv")
+          )
+          
+          var <- httr::POST(serverService, body=params)
+          code <- httr::content(var, "text")
+          
+          if (code == "200")
+            print("uploaded")
+          else
+            print("Not uploaded")
+          
+        }
+     
+        if(design == "Factorial Two-Way Design in RCBD (F2RCBD)"){
+
+          servName =   "2frcbd"
+          serverFileDir <-"https://research.cip.cgiar.org/gtdms/hidap/hidap_sbase_reports/files/"
+          serverService <-"https://research.cip.cgiar.org/gtdms/hidap/hidap_sbase_reports/getFileUpload.php"
+          
+          uploadDate  <- as.character(Sys.time(), "%Y%m%d%H%M%S")
+          ranStr <-  stringi::stri_rand_strings(1, 15,  '[a-zA-Z0-9]')
+          servName <- paste(uploadDate, ranStr, servName , sep= "-") #nombre sin extensions!!!!
+          
+          dirName <- fbglobal::get_base_dir()
+          print(dirName)
+          path <- paste0(dirName, servName, ".docx")
+          
+          pepa::repo.2f(data = fieldbook, traits = trait, 
+                        A = factor_single , B = factor2_single,
+                        rep = rep, design = "rcbd",format = format, 
+                        server =TRUE, server_dir_name = dirName, server_file_name = servName )
+          
+          
+          params <- list(
+            dataRequest = "uploadFile",
+            fileServerName = paste0(servName, ".docx"),
+            filedata= httr::upload_file(path, "text/csv")
+          )
+          
+          var <- httr::POST(serverService, body=params)
+          code <- httr::content(var, "text")
+          
+          
+          
+          if (code == "200")
+            print("uploaded")
+          else
+            print("Not uploaded")
+          
+        }
+        
         # if(design == "Split Plot with Plots in RCBD (SPRCBD)"){
         #   title <- paste("Automatic report for ", design, sep= "")
         #   try(pepa::repo.2f(traits = trait, A = genotypes, B = factor_single, rep = rep, design = "rcbd", title= title, data = fieldbook, format = format))
         #   path <- "/usr/local/lib/R/site-library/pepa/rmd/2frcbd.docx"
         #   #file.copy("/usr/local/lib/R/site-library/pepa/rmd/2frcbd.docx", con)
         # }
-        # 
-        # 
-        # if(design == "Factorial Two-Way Design in RCBD (F2RCBD)"){
-        #   
-        #   title <- paste("Automatic report for ", design, sep= "")
-        #   try(pepa::repo.2f(traits = trait, A = genotypes, B = factor_single, rep = rep, design = "rcbd", title= title, data = fieldbook, format = format))
-        #   path <- "/usr/local/lib/R/site-library/pepa/rmd/2frcbd.docx"
-        #   #file.copy("/usr/local/lib/R/site-library/pepa/rmd/2frcbd.docx", con)
-        # }
-        # 
         
+        # if(design == "Split Plot with Plots in CRD (SPCRD)"){
+        #   title <- paste("Automatic report for ", design, sep= "")
+        #   try(pepa::repo.2f(traits = trait, A = genotypes, B = factor_single, rep = rep, design = "crd",  title= title, data = fieldbook, format = format))
+        #   path <- "/usr/local/lib/R/site-library/pepa/rmd/2fcrd.docx"
+        #   #file.copy("/usr/local/lib/R/site-library/pepa/rmd/2fcrd.docx", con)
+        # }
         
         Sys.chmod(path, mode = "0777", use_umask = TRUE)
         
-        #print(paste0(serverFileDir, servName, ".docx"))
         print(servName)
-        # file.copy(paste0(serverFileDir, servName) , con, overwrite = TRUE)
         download.file(paste0(serverFileDir, servName, ".docx"), con, method = "curl")
         
         incProgress(4/5, detail = paste("Formattting in ", "MS Word",sep= ""))
--- R/ui_single_hdagrofims.R
+++ R/ui_single_hdagrofims.R
@@ -29,13 +29,14 @@ single_hdagrofims_ui <- function(type = "tab", title = "Statistical Analysis for
                                                      #selectInput('trait_single', 'Select Trait', c(Choose='', single_select_options()   ), selectize=TRUE),
                                                      #selectInput('rep_single', 'Select Repetition', c(Choose='', names(iris)), selectize=TRUE),
                                                      #selectInput('genotypes_single', 'Select Treatment', c(Choose='', names(iris)), selectize=TRUE),
-                                                     selectInput(inputId = 'design_single_agrofims', label = 'Select statistical design of your experiment', choices = 
-                                                                   c("Completely Randomized Design (CRD)",
-                                                                     "Randomized Complete Block Design (RCBD)"),
+                                                     selectInput(inputId = 'design_single_agrofims', 
+                                                                 label = 'Select statistical design of your experiment', 
+                                                                 choices = c("Completely Randomized Design (CRD)",
+                                                                     "Randomized Complete Block Design (RCBD)",
                                                                      #"Augmented Block Design (ABD)",
                                                                      #"Alpha Design(0,1) (AD)",
-                                                                     #"Factorial Two-Way Design in CRD (F2CRD)",
-                                                                     #"Factorial Two-Way Design in RCBD (F2RCBD)"),
+                                                                     "Factorial Two-Way Design in CRD (F2CRD)",
+                                                                     "Factorial Two-Way Design in RCBD (F2RCBD)"),
                                                                  #"Split Plot with Plots in CRD (SPCRD)",
                                                                  #"Split Plot with Plots in RCBD (SPRCBD)"),
                                                                  selected = "Randomized Complete Block Design (RCBD)",
@@ -52,7 +53,33 @@ single_hdagrofims_ui <- function(type = "tab", title = "Statistical Analysis for
                                                      #   uiOutput("trait_single")
                                                      #   
                                                      # ),
+                                                    
+                                                     shiny::conditionalPanel(
+                                                       condition = "input.design_single_agrofims == 'Alpha Design(0,1) (AD)'|
+                                                       input.design_single_agrofims == 'Completely Randomized Design (CRD)'|
+                                                       input.design_single_agrofims == 'Randomized Complete Block Design (RCBD)'|
+                                                       input.design_single_agrofims == 'Augmented Block Design (ABD)'",          
+                                                       #input.design_single_agrofims == 'Factorial Two-Way Design in CRD (F2CRD)'|
+                                                       #input.design_single_agrofims == 'Factorial Two-Way Design in RCBD (F2RCBD)'",
+                                                       #input.design_single_agrofims == 'Split Plot with Plots in CRD (SPCRD)'| 
+                                                       #input.design_single_agrofims == 'Split Plot with Plots in RCBD (SPRCBD)'|
+                                                       uiOutput("genotypes_single_agrofims")#,                                                    
+                                                                                                                  
+                                                     ),             
                                                      
+                                                     shiny::conditionalPanel(
+                                                       condition = "input.design_single_agrofims == 'Alpha Design(0,1) (AD)'|
+                                                       input.design_single_agrofims == 'Completely Randomized Design (CRD)'|
+                                                       input.design_single_agrofims == 'Randomized Complete Block Design (RCBD)'|
+                                                       input.design_single_agrofims == 'Augmented Block Design (ABD)'|          
+                                                       input.design_single_agrofims == 'Factorial Two-Way Design in CRD (F2CRD)'|
+                                                       input.design_single_agrofims == 'Factorial Two-Way Design in RCBD (F2RCBD)'",
+                                                       #input.design_single_agrofims == 'Split Plot with Plots in CRD (SPCRD)'| 
+                                                       #input.design_single_agrofims == 'Split Plot with Plots in RCBD (SPRCBD)'|
+                                                       #uiOutput("genotypes_single_agrofims")#,                                                    
+                                                       uiOutput("trait_single_agrofims")
+                                                    ),
+                                                      
                                                      shiny::conditionalPanel(                                                               
                                                        condition = "input.design_single_agrofims == 'Alpha Design(0,1) (AD)'|                      
                                                        input.design_single_agrofims == 'Randomized Complete Block Design (RCBD)'|
@@ -73,10 +100,17 @@ single_hdagrofims_ui <- function(type = "tab", title = "Statistical Analysis for
                                                        # input.design_single == 'Split Plot with Plots in CRD (SPCRD)'|
                                                        # input.design_single == 'Split Plot with Plots in RCBD (SPRCBD)'|
                                                        
-                                                       
                                                        uiOutput("factor_single_agrofims")
                                                      ),
                                                      
+                                                     shiny::conditionalPanel(
+                                                       condition =  "input.design_single_agrofims == 'Factorial Two-Way Design in CRD (F2CRD)'|
+                                                       input.design_single_agrofims == 'Factorial Two-Way Design in RCBD (F2RCBD)'",
+                                                       # input.design_single == 'Split Plot with Plots in CRD (SPCRD)'|
+                                                       # input.design_single == 'Split Plot with Plots in RCBD (SPRCBD)'|
+                                                       
+                                                       uiOutput("factor2_single_agrofims")
+                                                     ),
                                                      
                                                      
                                                      shiny::conditionalPanel(
@@ -88,34 +122,6 @@ single_hdagrofims_ui <- function(type = "tab", title = "Statistical Analysis for
                                                      ),
                                                      
                                                      
-                                                     
-                                                     shiny::conditionalPanel(
-                                                       condition = "input.design_single_agrofims == 'Alpha Design(0,1) (AD)'|
-                                                       input.design_single_agrofims == 'Completely Randomized Design (CRD)'|
-                                                       input.design_single_agrofims == 'Randomized Complete Block Design (RCBD)'|
-                                                       input.design_single_agrofims == 'Augmented Block Design (ABD)'|          
-                                                       input.design_single_agrofims == 'Factorial Two-Way Design in CRD (F2CRD)'|
-                                                       input.design_single_agrofims == 'Factorial Two-Way Design in RCBD (F2RCBD)'",
-                                                       #input.design_single_agrofims == 'Split Plot with Plots in CRD (SPCRD)'| 
-                                                       #input.design_single_agrofims == 'Split Plot with Plots in RCBD (SPRCBD)'|
-                                                       
-                                                       
-                                                       uiOutput("genotypes_single_agrofims"),                                                    
-                                                       uiOutput("trait_single_agrofims")                                                            
-                                                     ),                                                                                   
-                                                     
-                                                     
-                                                     
-                                                     #uiOutput("genotypes_single_agrofims"),
-                                                     #uiOutput("rep_single_agrofims"),
-                                                     
-                                                     #uiOutput("trait_single_agrofims"),
-                                                     
-                                                     
-                                                     # radioButtons(inputId="format_single_agrofims", label="Report format", choices= c("html","word"), 
-                                                     #              selected = "word", inline = TRUE, width = NULL),
-                                                      # actionButton(inputId = "downloadagrofims_single_report ", label= "Analyze", icon = icon("play-circle"),
-                                                      #             width = NULL,height = NULL),
                                                       # 
                                                      downloadButton(outputId = "downloadagrofims_single_report", label = "Download"),
                                                      
